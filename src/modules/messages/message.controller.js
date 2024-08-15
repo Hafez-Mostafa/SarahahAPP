ef3ed202197413def436fdf0ad3f382cbcac1225
Mostafa <mostafa.hafez.de@gmail.com>
2024-08-15 22:34:01 +0200

    'fast_fertig'

--- src/modules/messages/message.controller.js
+++ src/modules/messages/message.controller.js
@@ -1,5 +1,5 @@
 import messageModel from '../../../db/model/message.model.js'
-
+import QRCode from 'qrcode'
 
 
 
@@ -13,14 +13,20 @@ export const sendMessage = async (req, res, next) => {
 
 
 export const getprofile = async (req, res, next) => {
-
+    let url = `${req.protocol}://${req.get('host')}/others/user/${req.session.userId}`
+    let qrCodeUrl;
+    QRCode.toDataURL(url)
+    .then(url=>{
+        qrCodeUrl=url
+    }).catch(err=>{
+        console.error(err)
+    })
     try {
 
-        let url = `${req.protocol}://${req.get('host')}`
 
         const messages = await messageModel.find({ user: req.session.userId });
         if (req.session.loggedIn) {
-            res.render('profile.ejs', { session: req.session, messages, url });
+            res.render('profile.ejs', { session: req.session, messages, url,qrCodeUrl });
         } else {
             res.redirect('/messages/profile', { error: "You are not Authenticated" });
         }
